232e69083142bfaed506c3d3104e3f8271a08878
cort-robinson <cortrobinson99@gmail.com>
2020-07-21 15:09:33 +0000

    create _printf.c

---
+++ _printf.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _printf - Produces output according to a format.
+ * 
+ * @format: Format for output.
+ * 
+ * Return: Number of characters printed (exluding the 
+ * null byte used to end output to strings).
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
