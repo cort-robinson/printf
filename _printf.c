39c517127b796cc4cee8d068631fcccf5cfc5921
JHS1790 <1790@holbertonschool.com>
2020-07-21 16:19:47 +0000

    Flushed out prelim printf file

--- _printf.c
+++ _printf.c
@@ -2,14 +2,47 @@
 
 /**
  * _printf - Produces output according to a format.
- * 
+ *
  * @format: Format for output.
- * 
- * Return: Number of characters printed (exluding the 
+ *
+ * Return: Number of characters printed (exluding the
  * null byte used to end output to strings).
  */
 
 int _printf(const char *format, ...)
 {
 
+	va_list print;
+	int i, count = 0;
+
+	va_start(print, format);
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+			case 'c':
+				count += printchar(va_arg(print, char));
+				break;
+			case 'i':
+				count += printint(va_arg(print, int));
+				break;
+			case 'd':
+				count += printdec((float) va_arg(print, double));
+				break;
+			case '%':
+				count += _putchar('%');
+				break;
+			case 's':
+				count += printstring(va_arg(print, char *));
+				break;
+			}
+		}
+		else
+			count += _putchar(format[i]);
+	}
+	va_end(print);
+	return(count)
 }
